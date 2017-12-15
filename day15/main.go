eb67716ba2f6e08b56bb724be008b1bddb33c455
Kabir Sala <kabirsala@airmail.cc>
2017-12-15 12:55:14 +0100

    Solve part 2

--- day15/main.go
+++ day15/main.go
@@ -7,10 +7,12 @@ import (
 
 const (
 	// Generator A
-	factorA     = 16807
+	factorA = 16807
+	//startValueA = 65
 	startValueA = 699
 	// Generator B
-	factorB     = 48271
+	factorB = 48271
+	//startValueB = 8921
 	startValueB = 124
 )
 
@@ -20,24 +22,54 @@ func generate(previousValue int, factor int) int {
 
 func intToBin(value int) string {
 	s := strconv.FormatInt(int64(value), 2)
-	for len(s) != 32 {
-		s = fmt.Sprintf("0%s", s)
-	}
+	s = fmt.Sprintf("%032s", s)
 	return s
 }
 
-func main() {
-	previousValueA := startValueA
-	previousValueB := startValueB
-	var matches int
-	for i := 0; i <= 40000000; i++ {
-		valueA := generate(previousValueA, factorA)
-		valueB := generate(previousValueB, factorB)
-		if intToBin(valueA)[16:32] == intToBin(valueB)[16:32] {
-			matches++
+func generator(startValue int, factor int, multiple int, c chan int, done chan struct{}) {
+	value := startValue
+	defer close(c)
+	for {
+		value = generate(value, factor)
+		if (value % multiple) == 0 {
+			select {
+			case c <- value:
+				// pass
+			case <-done:
+				return
+			}
 		}
-		previousValueA = valueA
-		previousValueB = valueB
 	}
-	fmt.Println(matches)
+}
+
+func main() {
+	// Part 1
+	var matches1 int
+	cA := make(chan int, 64)
+	cB := make(chan int, 64)
+	done1 := make(chan struct{})
+	go generator(startValueA, factorA, 1, cA, done1)
+	go generator(startValueB, factorB, 1, cB, done1)
+	for i := 0; i <= 40000000; i++ {
+		if intToBin(<-cA)[16:32] == intToBin(<-cB)[16:32] {
+			matches1++
+		}
+	}
+	close(done1)
+	fmt.Printf("Matches first part: %d\n", matches1)
+
+	// Part 2
+	var matches2 int
+	c2A := make(chan int, 64)
+	c2B := make(chan int, 64)
+	done2 := make(chan struct{})
+	go generator(startValueA, factorA, 4, c2A, done2)
+	go generator(startValueB, factorB, 8, c2B, done2)
+	for i := 0; i <= 5000000; i++ {
+		if intToBin(<-c2A)[16:32] == intToBin(<-c2B)[16:32] {
+			matches2++
+		}
+	}
+	close(done2)
+	fmt.Printf("Matches second part: %d\n", matches2)
 }
